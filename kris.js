77aefdba80f5db06e69bee0d5ef9ad477764157a
Kris <kris@example.com>
2025-12-04 23:39:15 -0300

    Simplify JavaScript - remove debug logging

--- kris.js
+++ kris.js
@@ -1,23 +1,20 @@
 (function() {
     'use strict';
     
-    console.log('[KRIS] Script loaded successfully');
-    
-    // Inject custom CSS from GitHub Pages
+    // Inject custom CSS
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://krisenigma.github.io/socialstream-custom/kris.css';
     document.head.appendChild(link);
-    console.log('[KRIS] CSS injected');
     
+    const twitchImageUrl = 'https://krisenigma.github.io/socialstream-custom/twitchbg.png';
     const originalSrcDescriptor = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src');
     
+    // Replace Twitch icons on src property change
     Object.defineProperty(HTMLImageElement.prototype, 'src', {
         set: function(value) {
-            if (value && (value.includes('socialstream.ninja/sources/images/twitch.png') || value.includes('./sources/images/twitch.png'))) {
-                console.log('[KRIS] Replacing Twitch icon:', value);
-                // Replace with GitHub Pages-hosted image
-                originalSrcDescriptor.set.call(this, 'https://krisenigma.github.io/socialstream-custom/twitchbg.png');
+            if (value && value.includes('twitch.png')) {
+                originalSrcDescriptor.set.call(this, twitchImageUrl);
             } else {
                 originalSrcDescriptor.set.call(this, value);
             }
@@ -27,27 +24,23 @@
         }
     });
     
+    // Replace existing Twitch icons
     function replaceTwitchIcons() {
-        const icons = document.querySelectorAll('img[src*="twitch.png"]');
-        console.log('[KRIS] Found', icons.length, 'Twitch icons to replace');
-        icons.forEach(img => {
-            img.src = 'https://krisenigma.github.io/socialstream-custom/twitchbg.png';
+        document.querySelectorAll('img[src*="twitch.png"]').forEach(img => {
+            img.src = twitchImageUrl;
         });
     }
     
-    const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            mutation.addedNodes.forEach(function(node) {
-                if (node.nodeType === 1) { // Element node
-                    if (node.tagName === 'IMG' && node.src && node.src.includes('twitch.png')) {
-                        console.log('[KRIS] Replacing Twitch icon in new node');
-                        node.src = 'https://krisenigma.github.io/socialstream-custom/twitchbg.png';
-                    }
-                    node.querySelectorAll && node.querySelectorAll('img[src*="twitch.png"]').forEach(img => {
-                        console.log('[KRIS] Replacing Twitch icon in child');
-                        img.src = 'https://krisenigma.github.io/socialstream-custom/twitchbg.png';
-                    });
+    // Watch for new Twitch icons
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            mutation.addedNodes.forEach(node => {
+                if (node.nodeType === 1 && node.tagName === 'IMG' && node.src?.includes('twitch.png')) {
+                    node.src = twitchImageUrl;
                 }
+                node.querySelectorAll?.('img[src*="twitch.png"]').forEach(img => {
+                    img.src = twitchImageUrl;
+                });
             });
         });
     });
@@ -56,11 +49,9 @@
         observer.observe(document.body, { childList: true, subtree: true });
         replaceTwitchIcons();
     } else {
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', () => {
             observer.observe(document.body, { childList: true, subtree: true });
             replaceTwitchIcons();
         });
     }
-    
-    console.log('[KRIS] Script initialization complete');
 })();
