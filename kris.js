f8abf2c01493a5c92619b54e18ab040bf1198413
Kris <kris@example.com>
2025-12-04 22:00:46 -0300

    Update paths to use KrisEnigma GitHub account

--- kris.js
+++ kris.js
@@ -6,7 +6,7 @@
     // Inject custom CSS from GitHub
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/kris.css';
+    link.href = 'https://raw.githubusercontent.com/KrisEnigma/socialstream-custom/main/kris.css';
     document.head.appendChild(link);
     console.log('[KRIS] CSS injected');
     
@@ -17,7 +17,7 @@
             if (value && value.includes('socialstream.ninja/sources/images/twitch.png')) {
                 console.log('[KRIS] Replacing Twitch icon:', value);
                 // Replace with GitHub-hosted image
-                originalSrcDescriptor.set.call(this, 'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/twitchbg.png');
+                originalSrcDescriptor.set.call(this, 'https://raw.githubusercontent.com/KrisEnigma/socialstream-custom/main/twitchbg.png');
             } else {
                 originalSrcDescriptor.set.call(this, value);
             }
@@ -31,7 +31,7 @@
         const icons = document.querySelectorAll('img[src*="socialstream.ninja/sources/images/twitch.png"]');
         console.log('[KRIS] Found', icons.length, 'Twitch icons to replace');
         icons.forEach(img => {
-            img.src = 'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/twitchbg.png';
+            img.src = 'https://raw.githubusercontent.com/KrisEnigma/socialstream-custom/main/twitchbg.png';
         });
     }
     
@@ -41,11 +41,11 @@
                 if (node.nodeType === 1) { // Element node
                     if (node.tagName === 'IMG' && node.src && node.src.includes('socialstream.ninja/sources/images/twitch.png')) {
                         console.log('[KRIS] Replacing Twitch icon in new node');
-                        node.src = 'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/twitchbg.png';
+                        node.src = 'https://raw.githubusercontent.com/KrisEnigma/socialstream-custom/main/twitchbg.png';
                     }
                     node.querySelectorAll && node.querySelectorAll('img[src*="socialstream.ninja/sources/images/twitch.png"]').forEach(img => {
                         console.log('[KRIS] Replacing Twitch icon in child');
-                        img.src = 'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/twitchbg.png';
+                        img.src = 'https://raw.githubusercontent.com/KrisEnigma/socialstream-custom/main/twitchbg.png';
                     });
                 }
             });
